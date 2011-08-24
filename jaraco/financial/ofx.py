e65ff03997bd8f8e2570d5bcbcd21db094690aea
Jason R. Coombs <jaraco@jaraco.com>
2011-08-23 22:09:03 -0600

    Removed ofx.py (which was apparently just a predecessor of ofx_ba.py). Renamed ofx_ba to ofx.

--- jaraco/financial/ofx.py
+++ jaraco/financial/ofx.py
@@ -1,182 +1,248 @@
 #!/usr/bin/python
-import time, os, httplib, urllib2, uuid
+import time
+import os
+import httplib
+import urllib2
+import uuid
 import sys
+import argparse
+import getpass
+import itertools
+import collections
 
-join = str.join
+from jaraco.util.string import local_format as lf
 
 sites = {
-       "ucard": {
-                 "caps": [ "SIGNON", "CCSTMT" ],
-                  "fid": "24909",
-                "fiorg": "Citigroup",
-                  "url": "https://secureofx2.bankhost.com/citi/cgi-forte/ofx_rt?servicename=ofx_rt&pagename=ofx",
-                },
-    "discover": {
-                 "caps": [ "SIGNON", "CCSTMT" ],
-                "fiorg": "Discover Financial Services",
-                  "fid": "7101",
-                  "url": "https://ofx.discovercard.com/",
-                },
-     "ameritrade": {
-                 "caps": [ "SIGNON", "INVSTMT" ],
-                "fiorg": "ameritrade.com",
-                  "url": "https://ofx.ameritrade.com/ofxproxy/ofx_proxy.dll",
-                }
-    }
+	"SLFCU": {
+		"caps": ["SIGNON", "BASTMT"],
+		"fid": "1001",
+		"fiorg": "SLFCU",
+		"url": "https://www.cu-athome.org/scripts/serverext.dll",
+		"bankid": "307083911",
+	},
+}
 
-def _field(tag,value):
-    return "<"+tag+">"+value
+def _field(tag, value):
+	return lf('<{tag}>{value}')
 
-def _tag(tag,*contents):
-    return join("\r\n",["<"+tag+">"]+list(contents)+["</"+tag+">"])
+def _tag(tag, *contents):
+	start_tag = lf('<{tag}>')
+	end_tag = lf('</{tag}>')
+	lines = itertools.chain([start_tag], contents, [end_tag])
+	return '\r\n'.join(lines)
 
 def _date():
-    return time.strftime("%Y%m%d%H%M%S",time.localtime())
+	return time.strftime("%Y%m%d%H%M%S", time.localtime())
 
 def _genuuid():
-    return uuid.uuid4().hex
+	return uuid.uuid4().hex
 
-class OFXClient:
-    """Encapsulate an ofx client, config is a dict containg configuration"""
-    def __init__(self, config, user, password):
-        self.password = password
-        self.user = user
-        self.config = config
-        self.cookie = 3
-        config["user"] = user
-        config["password"] = password
-        if not config.has_key("appid"):
-            config["appid"] = "PyOFX"
-            config["appver"] = "0100"
+AppInfo = collections.namedtuple('AppInfo', 'id version')
 
-    def _cookie(self):
-        self.cookie += 1
-        return str(self.cookie)
+class OFXClient(object):
+	"""
+	Encapsulate an ofx client, config is a dict containg configuration.
+	"""
 
-    """Generate signon message"""
-    def _signOn(self):
-        config = self.config
-        fidata = [ _field("ORG",config["fiorg"]) ]
-        if config.has_key("fid"):
-            fidata += [ _field("FID",config["fid"]) ]
-        return _tag("SIGNONMSGSRQV1",
-                    _tag("SONRQ",
-                         _field("DTCLIENT",_date()),
-                         _field("USERID",config["user"]),
-                         _field("USERPASS",config["password"]),
-                         _field("LANGUAGE","ENG"),
-                         _tag("FI", *fidata),
-                         _field("APPID",config["appid"]),
-                         _field("APPVER",config["appver"]),
-                         ))
+	# set up some app ids
+	app = AppInfo('PyOFX', '0100')
+	# if you have problems, fake quicken with one of these app ids
+	quicken_2009 = AppInfo('QWIN', '1800')
+	quicken_older = AppInfo('QWIN', '1200')
 
-    def _acctreq(self, dtstart):
-        req = _tag("ACCTINFORQ",_field("DTACCTUP",dtstart))
-        return self._message("SIGNUP","ACCTINFO",req)
+	def __init__(self, config, user, password):
+		self.password = password
+		self.user = user
+		self.config = config
+		self.cookie = 3
+		config["user"] = user
+		config["password"] = password
+		config.setdefault('appid', self.app.id)
+		config.setdefault('appver', self.app.version)
 
-    def _ccreq(self, acctid, dtstart):
-        config=self.config
-        req = _tag("CCSTMTRQ",
-                   _tag("CCACCTFROM",_field("ACCTID",acctid)),
-                   _tag("INCTRAN",
-                        _field("DTSTART",dtstart),
-                        _field("INCLUDE","Y")))
-        return self._message("CREDITCARD","CCSTMT",req)
+	def _cookie(self):
+		self.cookie += 1
+		return str(self.cookie)
 
-    def _invstreq(self, brokerid, acctid, dtstart):
-        dtnow = time.strftime("%Y%m%d%H%M%S",time.localtime())
-        req = _tag("INVSTMTRQ",
-                   _tag("INVACCTFROM",
-                      _field("BROKERID", brokerid),
-                      _field("ACCTID",acctid)),
-                   _tag("INCTRAN",
-                        _field("DTSTART",dtstart),
-                        _field("INCLUDE","Y")),
-                   _field("INCOO","Y"),
-                   _tag("INCPOS",
-                        _field("DTASOF", dtnow),
-                        _field("INCLUDE","Y")),
-                   _field("INCBAL","Y"))
-        return self._message("INVSTMT","INVSTMT",req)
+	def _signOn(self):
+		"""Generate signon message"""
+		config = self.config
+		fidata = [_field("ORG", config["fiorg"])]
+		if config.has_key("fid"):
+			fidata += [_field("FID", config["fid"])]
+		return _tag("SIGNONMSGSRQV1",
+			_tag("SONRQ",
+				_field("DTCLIENT", _date()),
+				_field("USERID", config["user"]),
+				_field("USERPASS", config["password"]),
+				_field("LANGUAGE", "ENG"),
+				_tag("FI", *fidata),
+				_field("APPID", config["appid"]),
+				_field("APPVER", config["appver"]),
+			),
+		)
 
-    def _message(self,msgType,trnType,request):
-        config = self.config
-        return _tag(msgType+"MSGSRQV1",
-                    _tag(trnType+"TRNRQ",
-                         _field("TRNUID",_genuuid()),
-                         _field("CLTCOOKIE",self._cookie()),
-                         request))
+	def _acctreq(self, dtstart):
+		req = _tag("ACCTINFORQ", _field("DTACCTUP", dtstart))
+		return self._message("SIGNUP", "ACCTINFO", req)
 
-    def _header(self):
-        return join("\r\n",[ "OFXHEADER:100",
-                           "DATA:OFXSGML",
-                           "VERSION:102",
-                           "SECURITY:NONE",
-                           "ENCODING:USASCII",
-                           "CHARSET:1252",
-                           "COMPRESSION:NONE",
-                           "OLDFILEUID:NONE",
-                           "NEWFILEUID:"+_genuuid(),
-                           ""])
+	# this is from _ccreq below and reading page 176 of the latest OFX doc.
+	def _bareq(self, acctid, dtstart, accttype):
+		req = _tag("STMTRQ",
+			_tag("BANKACCTFROM",
+				_field("BANKID", sites[args.site]["bankid"]),
+				_field("ACCTID", acctid),
+				_field("ACCTTYPE", accttype),
+			),
+			_tag("INCTRAN",
+				_field("DTSTART", dtstart),
+				_field("INCLUDE", "Y"),
+			),
+		)
+		return self._message("BANK", "STMT", req)
 
-    def ccQuery(self, acctid, dtstart):
-        """CC Statement request"""
-        return join("\r\n",[self._header(),
-                          _tag("OFX",
-                               self._signOn(),
-                               self._ccreq(acctid, dtstart))])
+	def _ccreq(self, acctid, dtstart):
+		req = _tag("CCSTMTRQ",
+			_tag("CCACCTFROM", _field("ACCTID", acctid)),
+			_tag("INCTRAN",
+				_field("DTSTART", dtstart),
+				_field("INCLUDE", "Y"),
+			),
+		)
+		return self._message("CREDITCARD", "CCSTMT", req)
 
-    def acctQuery(self,dtstart):
-        return join("\r\n",[self._header(),
-                          _tag("OFX",
-                               self._signOn(),
-                               self._acctreq(dtstart))])
+	def _invstreq(self, brokerid, acctid, dtstart):
+		dtnow = time.strftime("%Y%m%d%H%M%S", time.localtime())
+		req = _tag("INVSTMTRQ",
+			_tag("INVACCTFROM",
+				_field("BROKERID", brokerid),
+				_field("ACCTID", acctid),
+			),
+			_tag("INCTRAN",
+				_field("DTSTART", dtstart),
+				_field("INCLUDE", "Y"),
+			),
+			_field("INCOO", "Y"),
+			_tag("INCPOS",
+				_field("DTASOF", dtnow),
+				_field("INCLUDE", "Y"),
+			),
+			_field("INCBAL", "Y"),
+		)
+		return self._message("INVSTMT","INVSTMT",req)
 
-    def invstQuery(self, brokerid, acctid, dtstart):
-        return join("\r\n",[self._header(),
-                          _tag("OFX",
-                               self._signOn(),
-                               self._invstreq(brokerid, acctid,dtstart))])
+	def _message(self, msgType, trnType, request):
+		return _tag(msgType + "MSGSRQV1",
+			_tag(trnType + "TRNRQ",
+				_field("TRNUID", _genuuid()),
+				_field("CLTCOOKIE", self._cookie()),
+				request,
+			),
+		)
 
-    def doQuery(self,query,name):
-        # N.B. urllib doesn't honor user Content-type, use urllib2
-        request = urllib2.Request(self.config["url"],
-                                  query,
-                                  { "Content-type": "application/x-ofx",
-                                    "Accept": "*/*, application/x-ofx"
-                                  })
-        if 1:
-            f = urllib2.urlopen(request)
-            response = f.read()
-            f.close()
+	def _header(self):
+		return '\r\n'.join([
+			"OFXHEADER:100",
+			"DATA:OFXSGML",
+			"VERSION:102",
+			"SECURITY:NONE",
+			"ENCODING:USASCII",
+			"CHARSET:1252",
+			"COMPRESSION:NONE",
+			"OLDFILEUID:NONE",
+			"NEWFILEUID:" + _genuuid(),
+			"",
+		])
 
-            f = file(name,"w")
-            f.write(response)
-            f.close()
+	def baQuery(self, acctid, dtstart, accttype):
+		"""Bank account statement request"""
+		return '\r\n'.join([
+			self._header(),
+			_tag("OFX",
+				self._signOn(),
+				self._bareq(acctid, dtstart, accttype),
+			),
+		])
+
+	def ccQuery(self, acctid, dtstart):
+		"""CC Statement request"""
+		return '\r\n'.join([
+			self._header(),
+			_tag("OFX",
+				self._signOn(),
+				self._ccreq(acctid, dtstart),
+			),
+		])
+
+	def acctQuery(self,dtstart):
+		return '\r\n'.join([
+			self._header(),
+			_tag("OFX",
+				self._signOn(),
+				self._acctreq(dtstart),
+			),
+		])
+
+	def invstQuery(self, brokerid, acctid, dtstart):
+		return '\r\n'.join([
+			self._header(),
+			_tag("OFX",
+				self._signOn(),
+				self._invstreq(brokerid, acctid,dtstart),
+			),
+		])
+
+	def doQuery(self,query,name):
+		headers = {
+			"Content-type": "application/x-ofx",
+			"Accept": "*/*, application/x-ofx",
+		}
+		request = urllib2.Request(
+			self.config["url"],
+			query,
+			headers,
+		)
+		if 1:
+			f = urllib2.urlopen(request)
+			response = f.read()
+			f.close()
+
+			f = file(name, "w")
+			f.write(response)
+			f.close()
+		else:
+			print request
+			print self.config["url"], query
+
+def get_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('site', help="One of {0}".format(', '.join(sites)))
+	parser.add_argument('-u', '--username', default=getpass.getuser())
+	parser.add_argument('-a', '--account')
+	parser.add_argument('-t', '--account-type',
+		help="Required if retrieving bank statement, should be CHECKING, SAVINGS, ...",
+	)
+	globals().update(args = parser.parse_args())
+
+def handle_command_line():
+	get_args()
+	dtstart = time.strftime("%Y%m%d",time.localtime(time.time()-31*86400))
+	dtnow = time.strftime("%Y%m%d",time.localtime())
+	passwd = getpass.getpass()
+	client = OFXClient(sites[args.site], args.username, passwd)
+	if not args.account:
+		query = client.acctQuery("19700101000000")
+		client.doQuery(query, args.site+"_acct.ofx")
 	else:
-            print request
-            print self.config["url"], query
+		caps = sites[args.site]['caps']
+		if "CCSTMT" in caps:
+			query = client.ccQuery(args.account, dtstart)
+		elif "INVSTMT" in caps:
+			query = client.invstQuery(sites[args.site]["fiorg"],
+				args.account, dtstart)
+		elif "BASTMT" in caps:
+			query = client.baQuery(args.account, dtstart, args.account_type)
+		client.doQuery(query, args.site+dtnow+".ofx")
 
-        # ...
-
-import getpass
-argv = sys.argv
 if __name__=="__main__":
-    dtstart = time.strftime("%Y%m%d",time.localtime(time.time()-31*86400))
-    dtnow = time.strftime("%Y%m%d",time.localtime())
-    if len(argv) < 3:
-        print "Usage:",sys.argv[0], "site user [account]"
-        print "available sites:",join(", ",sites.keys())
-        sys.exit()
-    passwd = getpass.getpass()
-    client = OFXClient(sites[argv[1]], argv[2], passwd)
-    if len(argv) < 4:
-       query = client.acctQuery("19700101000000")
-       client.doQuery(query, argv[1]+"_acct.ofx")
-    else:
-       if "CCSTMT" in sites[argv[1]]["caps"]:
-          query = client.ccQuery(sys.argv[3], dtstart)
-       elif "INVSTMT" in sites[argv[1]]["caps"]:
-          query = client.invstQuery(sites[argv[1]]["fiorg"], sys.argv[3], dtstart)
-       client.doQuery(query, argv[1]+dtnow+".ofx")
-
+	handle_command_line()
