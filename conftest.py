50478537b09d5f345a0279003e27fbc24d95cb74
Jason R. Coombs <jaraco@jaraco.com>
2019-05-27 15:40:23 -0400

    Fade to black

--- conftest.py
+++ conftest.py
@@ -3,7 +3,8 @@ import sys
 
 collect_ignore = []
 
-collect_ignore += [
-	'jaraco/financial/paychex.py',
-	'jaraco/financial/lifo.py',
-] if sys.version_info < (3,) else []
+collect_ignore += (
+    ['jaraco/financial/paychex.py', 'jaraco/financial/lifo.py']
+    if sys.version_info < (3,)
+    else []
+)
